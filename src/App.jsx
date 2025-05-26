df41058260a34b944b0a66ea35b347931dd569a3
AMARTYA149 <amartya970914@gmail.com>
2025-05-26 19:45:59 +0530

    S-2 Ep16 npm install @reduxjs/toolkit react-redu

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import Body from "./Body";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Profile from "./Profile";
+import { Provider } from "react-redux";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Body />}>
-            <Route path="/login" element={<Login />}></Route>
-            <Route path="/profile" element={<Profile />}></Route>
-
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Provider>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Body />}>
+              <Route path="/login" element={<Login />}></Route>
+              <Route path="/profile" element={<Profile />}></Route>
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
