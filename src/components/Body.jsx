e5a6b6853dad148f4b11a887f579556503e85c7b
AMARTYA149 <amartya970914@gmail.com>
2025-05-27 09:00:49 +0530

    S-2 Ep17 stay loggedin even after refresh page

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,30 @@
 import { Outlet } from "react-router-dom";
 import NavBar from "./NavBar";
 import Footer from "./footer";
+import { useDispatch } from "react-redux";
+import axios from "axios";
+import { BASE_URL } from "../utils/constants";
+import { addUser } from "../utils/userSlice";
+import { useEffect } from "react";
 
 const Body = () => {
+  const dispatch = useDispatch();
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div>
       <NavBar />
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -25,12 +25,18 @@ const NavBar = () => {
               tabIndex={0}
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
             >
-              <li>Profile</li>
-              <li>Connections</li>
-
-              <li>Requests</li>
-              <li>Premium</li>
-              <li>Logout</li>
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
             </ul>
           </div>
         </div>
